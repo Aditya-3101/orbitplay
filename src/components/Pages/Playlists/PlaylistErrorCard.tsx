7a0ae8d13478b3330b846757e23cc914c777c0f3
Aditya-3101 <adityadhayfule3101@gmail.com>
2026-08-14 09:31:18 +0000

    minor UI bug fix

--- src/components/Pages/Playlists/PlaylistErrorCard.tsx
+++ src/components/Pages/Playlists/PlaylistErrorCard.tsx
@@ -12,7 +12,7 @@ export const PlaylistErrorCard = memo(({playlistVideosLength,playlistVidsLength}
   return (
     <div>
         {playlistDiff.length>0&&playlistDiff.map((_,index)=>{
-            return<div key={index} className='grid grid-cols-[40%_60%] grid-rows-[7rem] md:grid-rows-[10rem] lg:grid-rows-[12rem] md:grid-cols-[40%_60%] lg:grid-cols-[35%_65%] gap-2 my-4 border border-gray-500'>
+            return<div key={index} className='grid grid-cols-[40%_60%] grid-rows-[7rem] md:grid-rows-[10rem] lg:grid-rows-[12rem] md:grid-cols-[40%_60%] lg:grid-cols-[35%_65%] xl:grid-cols-[30%_70%] lg:grid-rows-[12rem] md:grid-cols-[40%_60%] lg:grid-cols-[35%_65%] gap-2 my-4 border border-gray-500'>
             <section className='relative flex justify-center'>
                 <div className='w-full h-full aspect-video object-cover bg-[rgba(240,240,240,0.1)] flex items-center justify-center'>
                     <VideoOff color='rgb(140,140,140)' size={56} />
